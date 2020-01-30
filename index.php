bf599648e14554b182da1aa02f7ee74202e486a7
Rodrigo <rodrigo.mct@gmail.com>
2020-01-30 00:23:22 -0300

    Changend index remove banner e and code in tips.txt

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <!-- Header -->
 <?php get_header();?>
 
-<!-- Banner -->
-<?php get_template_part( 'template-parts/banner');?>
-
 <!-- Carousel Slick-->
 <?php get_template_part( 'template-parts/loop','noticias');?>
 
